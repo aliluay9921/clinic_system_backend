d5105b577e4797e69da7af92c4ef74d3d51456d0
aliluay992 <luaya577@gmail.com>
2023-05-26 19:40:14 +0300

    ali

--- app/Models/Employee.php
+++ app/Models/Employee.php
@@ -3,9 +3,10 @@
 namespace App\Models;
 
 use App\Traits\Uuids;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Employee extends Model
 {
@@ -13,8 +14,13 @@ class Employee extends Model
 
     protected $guarded = [];
     protected $date = ['deleted_at'];
+    protected $appends = ['amount_withdraw_salary'];
 
 
+    public function getAmountWithdrawSalaryAttribute()
+    {
+        return Log::where("target_id", $this->id)->where("log_type", 0)->whereBetween("created_at", [Carbon::now()->startOfMonth(), Carbon::now()->endOfMonth()])->sum("value");
+    }
     public function clinic()
     {
         return $this->belongsTo(Clinic::class, 'clinic_id');
--- app/Models/Representative.php
+++ app/Models/Representative.php
@@ -2,10 +2,11 @@
 
 namespace App\Models;
 
+use Carbon\Carbon;
 use App\Traits\Uuids;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Representative extends Model
 {
@@ -13,6 +14,13 @@ class Representative extends Model
 
     protected $guarded = [];
     protected $date = ["deleted_at"];
+    protected $appends = ['paided'];
+
+
+    public function getpaidedAttribute()
+    {
+        return Log::where("target_id", $this->id)->where("log_type", 0)->sum("value");
+    }
 
     public function clinic()
     {
